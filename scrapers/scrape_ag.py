2b3623b8580afea89228786ce671b1e8eb40db6c
Markus Meier <maekke@gentoo.org>
2020-08-14 16:24:43 +0200

    move scrape_zg to function to avoid pylint warning

--- scrapers/scrape_ag.py
+++ scrapers/scrape_ag.py
@@ -11,29 +11,35 @@ def parse_date(date_str):
     return arrow.get(date_str, 'D. MMMM YYYY', locale='de_CH').datetime
 
 
-URL = 'https://www.ag.ch/de/themen_1/coronavirus_2/lagebulletins/lagebulletins_1.jsp'
-content = sc.download(URL)
-
-soup = BeautifulSoup(content, 'html.parser')
-
-h2 = soup.find(string=re.compile('Verlauf Ansteckungsorte')).find_parent('h2')
-
-subcat = h2.find_next('div')
-categories = subcat.findChildren('h2')
-for category in categories:
-    cat = category.text
-    accordion = category.find_next('div')
-    table_wrapper = accordion.findChild('div')
     table = table_wrapper.findChild('table')
-    trs = table.findChildren('tr')
-    n = len(trs)
-    assert n > 0
-    # skip header tr
-    for i in range(1, n):
-        tr = trs[i]
-        tds = tr.findChildren('td')
-        isd = sc.InfectionSourceData('AG', URL)
-        isd.date = parse_date(tds[0].text).date().isoformat()
-        isd.source = cat
-        isd.count = tds[2].text
-        print(isd)
+def scrape_ag():
+    url = 'https://www.ag.ch/de/themen_1/coronavirus_2/lagebulletins/lagebulletins_1.jsp'
+    content = sc.download(url)
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    h2 = soup.find(string=re.compile('Verlauf Ansteckungsorte')).find_parent('h2')
+
+    subcat = h2.find_next('div')
+    categories = subcat.findChildren('h2')
+    for category in categories:
+        cat = category.text
+        accordion = category.find_next('div')
+        table_wrapper = accordion.findChild('div')
+        table = table_wrapper.findChild('table')
+        trs = table.findChildren('tr')
+        n = len(trs)
+        assert n > 0
+        # skip header tr
+        for i in range(1, n):
+            tr = trs[i]
+            tds = tr.findChildren('td')
+            isd = sc.InfectionSourceData('AG', url)
+            isd.date = parse_date(tds[0].text).date().isoformat()
+            isd.source = cat
+            isd.count = tds[2].text
+            print(isd)
+
+
+if __name__ == '__main__':
+    scrape_ag()
--- scrapers/scrape_zg.py
+++ scrapers/scrape_zg.py
@@ -12,28 +12,33 @@ def parse_date(date_str):
     return datetime.datetime.strptime(date_str, '%d.%m.%Y %H:%M')
 
 
-data = []
-date = None
+def scrape_zg():
+    data = []
+    date = None
 
-URL = 'https://www.zg.ch/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-05.csv'
-content = sc.download(URL)
-reader = csv.DictReader(StringIO(content), delimiter=',')
-for row in reader:
-    source = row['Ansteckungsquelle']
-    if source != 'NA':
-        isd = sc.InfectionSourceData('ZG', URL)
-        isd.source = source
-        isd.count = row['Anzahl']
-        data.append(isd)
+    url = 'https://www.zg.ch/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-05.csv'
+    content = sc.download(url)
+    reader = csv.DictReader(StringIO(content), delimiter=',')
+    for row in reader:
+        source = row['Ansteckungsquelle']
+        if source != 'NA':
+            isd = sc.InfectionSourceData('ZG', url)
+            isd.source = source
+            isd.count = row['Anzahl']
+            data.append(isd)
 
-    if row['Type'] == 'subtitle':
-        content = row['Content']
-        res = re.search('Datenstand: (.*)$', content)
-        if res:
-            date = parse_date(res[1])
+        if row['Type'] == 'subtitle':
+            content = row['Content']
+            res = re.search('Datenstand: (.*)$', content)
+            if res:
+                date = parse_date(res[1])
 
-assert date is not None
-for item in data:
-    item.date = date.date().isoformat()
-    item.time = date.time().isoformat()
-    print(item)
+    assert date is not None
+    for item in data:
+        item.date = date.date().isoformat()
+        item.time = date.time().isoformat()
+        print(item)
+
+
+if __name__ == '__main__':
+    scrape_zg()
