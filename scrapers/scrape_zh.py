54f3f33a5c5a6f61c24216f4ebfb9a6de59b7b6f
Markus Meier <maekke@gentoo.org>
2020-08-14 16:28:21 +0200

    move scrape_zh to function

--- scrapers/scrape_zh.py
+++ scrapers/scrape_zh.py
@@ -11,15 +11,20 @@ def parse_date(date_str):
     return datetime.datetime.strptime(date_str, '%Y-%m-%d').date().isoformat()
 
 
-URL = 'https://raw.githubusercontent.com/openZH/covid_19_contact_tracing_ZH/master/data/Ansteckungswege.csv'
-content = sc.download(URL)
-reader = csv.DictReader(StringIO(content), delimiter=',')
-for row in reader:
-    isd = sc.InfectionSourceData('ZH', URL)
-    isd.date_from = parse_date(row['from'])
-    isd.date_to = parse_date(row['until'])
-    isd.source = row['context_cat']
-    if isd.source == '':
-        isd.source = row['context_bool']
-    isd.count = row['n_conf']
-    print(isd)
+def scrape_zh():
+    url = 'https://raw.githubusercontent.com/openZH/covid_19_contact_tracing_ZH/master/data/Ansteckungswege.csv'
+    content = sc.download(url)
+    reader = csv.DictReader(StringIO(content), delimiter=',')
+    for row in reader:
+        isd = sc.InfectionSourceData('ZH', url)
+        isd.date_from = parse_date(row['from'])
+        isd.date_to = parse_date(row['until'])
+        isd.source = row['context_cat']
+        if isd.source == '':
+            isd.source = row['context_bool']
+        isd.count = row['n_conf']
+        print(isd)
+
+
+if __name__ == '__main__':
+    scrape_zh()
