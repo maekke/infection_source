9e1498266e4253ad33014451cae0738b07a5d131
Markus Meier <maekke@gentoo.org>
2020-09-28 21:39:32 +0200

    adjust to latest AG website changes
    would be better to parse the xls at some point...

--- scrapers/scrape_ag.py
+++ scrapers/scrape_ag.py
@@ -8,7 +8,7 @@ import scrape_common as sc
 
 
 def parse_ag_date(date_str):
-    return arrow.get(date_str, 'D. MMMM YYYY', locale='de_CH').datetime
+    return arrow.get(date_str, 'DD.MM.YYYY', locale='de_CH').datetime
 
 
 def scrape_ag():
@@ -16,22 +16,26 @@ def scrape_ag():
     content = sc.download(url)
     content = BeautifulSoup(content, 'html.parser')
 
-    subcat = content.find(string=re.compile('Verlauf Ansteckungsorte')).find_parent('h2').find_next('div')
-    for category in subcat.findChildren('h2'):
-        table_wrapper = category.find_next('div').findChild('div')
-        table = table_wrapper.findChild('table')
-        trs = table.findChildren('tr')
-        n_trs = len(trs)
-        assert n_trs > 0
-        # skip header tr
-        for i in range(1, n_trs):
-            row = trs[i]
-            tds = row.findChildren('td')
-            isd = sc.InfectionSourceData('AG', url)
-            isd.date = parse_ag_date(tds[0].text).date().isoformat()
-            isd.source = category.text
-            isd.count = tds[2].text
-            print(isd)
+    # get the date from the last row
+    table = content.find(string=re.compile('1. Daten zu Covid-19')).find_next('table')
+    rows = table.findChildren('tr')
+    tds = rows[-1].findChildren('td')
+    date = parse_ag_date(tds[0].text).date().isoformat()
+
+    # get the data
+    table = content.find(string=re.compile('ANSTECKUNGSORT')).find_parent('table')
+    trs = table.findChildren('tr')
+    n_trs = len(trs)
+    assert n_trs > 0
+    # skip header tr
+    for i in range(1, n_trs):
+        row = trs[i]
+        tds = row.findChildren('td')
+        isd = sc.InfectionSourceData('AG', url)
+        isd.date = date
+        isd.source = tds[0].text
+        isd.count = tds[1].text
+        print(isd)
 
 
 if __name__ == '__main__':
