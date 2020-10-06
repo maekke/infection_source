ee1d914952ead9affb03c58950dbd4e6946053fe
Markus Meier <maekke@gentoo.org>
2020-10-06 21:17:14 +0200

    update AG scraper to parse xls file

--- scrapers/scrape_ag.py
+++ scrapers/scrape_ag.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python
 
-import re
-import arrow
-from bs4 import BeautifulSoup
+import xlrd
 
 import scrape_common as sc
 
 
-def parse_ag_date(date_str):
-    return arrow.get(date_str, 'DD.MM.YYYY', locale='de_CH').datetime
-
-
 def scrape_ag():
-    url = 'https://www.ag.ch/de/themen_1/coronavirus_2/lagebulletins/lagebulletins_1.jsp'
-    content = sc.download(url)
-    content = BeautifulSoup(content, 'html.parser')
-
-    # get the date from the last row
-    table = content.find(string=re.compile('1. Daten zu Covid-19')).find_next('table')
-    rows = table.findChildren('tr')
-    tds = rows[-1].findChildren('td')
-    date = parse_ag_date(tds[0].text).date().isoformat()
-
-    # get the data
-    table = content.find(string=re.compile('ANSTECKUNGSORT')).find_parent('table')
-    trs = table.findChildren('tr')
-    n_trs = len(trs)
-    assert n_trs > 0
-    # skip header and total in tr
-    for i in range(1, n_trs - 1):
-        row = trs[i]
-        tds = row.findChildren('td')
-        isd = sc.InfectionSourceData('AG', url)
-        isd.date = date
-        isd.source = tds[0].text
-        isd.count = tds[1].text
-        print(isd)
+    url = 'https://www.ag.ch/media/kanton_aargau/themen_1/coronavirus_1/daten_excel/Covid-19-Daten_Kanton_Aargau.xlsx'
+    content = sc.download_content(url)
+    xls = xlrd.open_workbook(file_contents=content)
+    xls_datemode = xls.datemode
+    sheet = xls.sheet_by_name('3. Ansteckungsorte')
+    categories = {c: str(sheet.cell_value(1, c) or xlrd.formula.colname(c)) for c in range(1, sheet.ncols, 2)}
+    for r in range(56, sheet.nrows):
+        date = sheet.cell_value(r, 0)
+        if date == '':
+            return
+        date = xlrd.xldate_as_datetime(date, xls_datemode).date()
+        for col, cat in categories.items():
+            # TODO total count?
+            count = sheet.cell_value(r, col + 1)
+            if count != '':
+                count = int(count)
+                isd = sc.InfectionSourceData('AG', url)
+                isd.date = date.isoformat()
+                isd.source = cat
+                isd.count = str(count)
+                print(isd)
 
 
 if __name__ == '__main__':
